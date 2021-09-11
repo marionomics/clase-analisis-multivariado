e954aa255d35a6119c1aad1bce584f38c3d2940a
Mario A. Garcia-Meza <marius.agm@gmail.com>
2021-09-11 11:45:42 -0500

    Modelo mean shift

--- scripts/Python/meanshift.py
+++ scripts/Python/meanshift.py
@@ -4,4 +4,16 @@ from sklearn.cluster import MeanShift
 
 if __name__ == "__main__":
     dataset = pd.read_csv("./data/candy.csv")
+    #url = "https://raw.githubusercontent.com/marionomics/clase-analisis-multivariado/main/data/candy.csv"
+    #dataset = pd.read_csv(url)
     print(dataset.head(5))
+
+    X = dataset.drop('competitorname', axis=1)
+    meanshift = MeanShift().fit(X)
+    print(meanshift.labels_)
+    print("*"*64)
+    print(meanshift.cluster_centers_)
+
+    dataset['meanshift'] = meanshift.labels_
+    print("="*64)
+    print(dataset)
